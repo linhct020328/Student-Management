851079147c8afa2df620acd37d91e11921717021
linhct020328 <39751184+linhct020328@users.noreply.github.com>
2020-08-04 11:47:53 +0700

    Update search.php

--- search.php
+++ search.php
@@ -17,7 +17,7 @@
 <html>
 <head>
 	<title>Show Students</title>
-  <link rel="stylesheet" type="text/css" href="css/show.css">
+  <link rel="stylesheet" type="text/css" href="show.css">
 
 </head>
 <body>
